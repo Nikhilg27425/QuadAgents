dbf68d85b1742044085d22606ecc0120be4de412
prachis2312 <prachisaxena2312@gmail.com>
2026-03-07 18:44:26 +0530

    Update ProtectedRoute.tsx

--- frontend/src/components/Layout/ProtectedRoute.tsx
+++ frontend/src/components/Layout/ProtectedRoute.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useUser } from "@/context/UserContext";
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
 
-  const { isAuthenticated } = useUser();
   const token = localStorage.getItem("token");
 
-  if (!isAuthenticated && !token) {
-    return <Navigate to="/login" replace />;
-  }
+  if (!token) return <Navigate to="/login" replace />;
 
   return <>{children}</>;
 }
